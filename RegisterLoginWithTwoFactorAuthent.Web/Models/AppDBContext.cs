f986eef4075ae8ea8a7060c9d114a152241bd049
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-05-14 11:53:29 +0400

    create App User and Role, add sql string

---
+++ RegisterLoginWithTwoFactorAuthent.Web/Models/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace RegisterLoginWithTwoFactorAuthent.Web.Models
+{
+    public class AppDBContext: IdentityDbContext<AppUser,AppRole,string>
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
