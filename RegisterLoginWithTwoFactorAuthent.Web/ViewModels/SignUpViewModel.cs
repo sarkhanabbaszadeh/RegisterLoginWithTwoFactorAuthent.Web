20dfaf36cf1e262db289aa40360f0865129ce15d
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-05-15 17:02:51 +0400

    create signup view & viewmodels

---
+++ RegisterLoginWithTwoFactorAuthent.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegisterLoginWithTwoFactorAuthent.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel(string userName, string password, string email, string phoneNumber)
+        {
+            UserName = userName;
+            Password = password;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        [Display(Name ="İstifadəçi adı : ")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Şifrə : ")]
+        public string Password { get; set; }
+
+        [Display(Name = "Təkrar Şifrə : ")]
+        public string PasswordConfirm { get; set; }
+
+        [Display(Name = "Epoçta : ")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefon : ")]
+        public string PhoneNumber { get; set; }
+    }
+}
