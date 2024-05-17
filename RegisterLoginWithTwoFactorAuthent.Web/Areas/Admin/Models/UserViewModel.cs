c4d25d155b1bf89c62309eea291ddee0557ca928
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-05-17 17:08:48 +0400

    create User List method & models & add programs

---
+++ RegisterLoginWithTwoFactorAuthent.Web/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RegisterLoginWithTwoFactorAuthent.Web.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string? ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
