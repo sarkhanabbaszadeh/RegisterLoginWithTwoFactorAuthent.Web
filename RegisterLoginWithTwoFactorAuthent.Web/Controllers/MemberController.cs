4488c7f65cfa6615233231b4b10cdb76d1baf3c4
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2024-05-22 17:42:52 +0400

    add logout simple method & partial view

---
+++ RegisterLoginWithTwoFactorAuthent.Web/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RegisterLoginWithTwoFactorAuthent.Web.Models;
+
+namespace RegisterLoginWithTwoFactorAuthent.Web.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
